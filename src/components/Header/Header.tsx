255520bcfe4b5ac421e5ce6c54abd15a7d45841f
Ellinor Johansson <ellinor.johansson@medieinstitutet.se>
2025-08-25 18:29:11 +0200

    💄(style): add style and mobile functionality to header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,16 +1,39 @@
-import { NavLink } from "react-router"
+import { NavLink } from 'react-router';
+import { useState } from 'react';
+import './header.scss';
 
 export const Header = () => {
-    return (
-        <nav>
-            <ul>
-                <li>
-                    <NavLink to={"/"}>Home</NavLink>
-                </li>
-                <li>
-                    <NavLink to={"/animals"}>Animals</NavLink>
-                </li>
-            </ul>
-        </nav>
-    )
-}
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => setMenuOpen(!menuOpen);
+
+  return (
+    <header className={`header ${menuOpen ? 'open' : ''}`}>
+      <h1 className="logo">The ZOO</h1>
+
+      <button className="hamburger" onClick={toggleMenu}>
+        <span className="line"></span>
+        <span className="line"></span>
+        <span className="line"></span>
+        <span className="menu-text hamburger">
+          {menuOpen ? 'Close' : 'Menu'}
+        </span>
+      </button>
+
+      <nav className={`nav ${menuOpen ? 'open' : ''}`}>
+        <ul>
+          <li>
+            <NavLink to="/" onClick={toggleMenu}>
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/animals" onClick={toggleMenu}>
+              Animals
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,19 +1,15 @@
-import { Outlet } from "react-router"
-import { Header } from "../components/Header/Header"
-import { Footer } from "../components/Footer/Footer"
+import { Outlet } from 'react-router';
+import { Header } from '../components/Header/Header';
+import { Footer } from '../components/Footer/Footer';
 
 export const Layout = () => {
-    return (
-        <>
-            <header>
-                <Header/>
-            </header>
-            <main>
-                <Outlet/>
-            </main>
-            <footer>
-                <Footer/>
-            </footer>
-        </>
-    )
-}
+  return (
+    <>
+      <Header />
+
+      <Outlet />
+
+      <Footer />
+    </>
+  );
+};
