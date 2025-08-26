0591a21b38646326f9d537a057423f54afe749dc
Ellinor Johansson <ellinor.johansson@medieinstitutet.se>
2025-08-26 14:03:17 +0200

    ✨(feature): add hook for fetch animals

---
+++ src/hooks/useAnimalHook.ts
@@ -0,0 +1,32 @@
+import { useEffect, useContext } from "react";
+import { AnimalContext } from "../context/AnimalContext";
+import type { Animal } from "../models/Animal";
+
+export const useAnimalsService = () => {
+  const { animals, dispatch } = useContext(AnimalContext);
+
+  useEffect(() => {
+    if (animals.length > 0) return; 
+
+    const fetchAnimals = async () => {
+      try {
+        const res = await fetch("https://animals.azurewebsites.net/api/animals");
+        const data: Animal[] = await res.json();
+
+        // localStorage
+        const animalsWithLocalFed = data.map(animal => {
+          const localLastFed = localStorage.getItem(`lastFed-${animal.id}`);
+          return localLastFed
+            ? { ...animal, lastFed: localLastFed }
+            : animal;
+        });
+
+        dispatch({ type: "SET_ANIMALS", payload: animalsWithLocalFed });
+      } catch (err) {
+        console.error("Couldn't get animals", err);
+      }
+    };
+
+    fetchAnimals();
+  }, [animals, dispatch]);
+};
