e336f1c7b80ae40964582f72fc5f117e3b536113
Ellinor Johansson <ellinor.johansson@medieinstitutet.se>
2025-08-27 10:59:15 +0200

    💄(style): add style to animals and changed from green theme to blue

--- src/pages/Animals.tsx
+++ src/pages/Animals.tsx
@@ -1,8 +1,8 @@
-import { useContext } from "react";
-import { AnimalContext } from "../context/AnimalContext";
-import { useAnimalsService } from "../hooks/useAnimalHook";
-import { getAnimalStatus } from "../helpers/AnimalHelpers";
-import { Link } from "react-router";
+import { useContext } from 'react';
+import { AnimalContext } from '../context/AnimalContext';
+import { useAnimalsService } from '../hooks/useAnimalHook';
+import { getAnimalStatus } from '../helpers/AnimalHelpers';
+import { Link } from 'react-router';
 import '../style/animals.scss';
 
 export const Animals = () => {
@@ -14,19 +14,23 @@ export const Animals = () => {
   return (
     <div className="animals">
       <ul>
-        {animals.map(animal => (
+        {animals.map((animal) => (
           <li key={animal.id}>
             <Link to={`/animals/${animal.id}`}>
-              <div>
+              <div className="image-container">
                 <img
                   src={animal.imageUrl}
                   alt={animal.name}
-                  onError={e => (e.currentTarget.src = "")}
+                  onError={(e) => (e.currentTarget.src = '')}
                   width={200}
                 />
+              </div>
+              <div className='animal-info'>
                 <h2>{animal.name}</h2>
-                <p>{animal.shortDescription}</p>
-                <p className="status">Status: {getAnimalStatus(animal, { overview: true })}</p>
+                <p className='short-desc'>{animal.shortDescription}</p>
+                <p className="status">
+                  Status: {getAnimalStatus(animal, { overview: true })}
+                </p>
               </div>
             </Link>
           </li>
