38edee61397be0352f6496351812cdac4451716c
Ellinor Johansson <ellinor.johansson@medieinstitutet.se>
2025-08-27 11:21:38 +0200

    ✨(feature): add start text to animals page

--- src/pages/Animals.tsx
+++ src/pages/Animals.tsx
@@ -12,30 +12,34 @@ export const Animals = () => {
   useAnimalsService();
 
   return (
-    <div className="animals">
-      <ul>
-        {animals.map((animal) => (
-          <li key={animal.id}>
-            <Link to={`/animals/${animal.id}`}>
-              <div className="image-container">
-                <img
-                  src={animal.imageUrl}
-                  alt={animal.name}
-                  onError={(e) => (e.currentTarget.src = '')}
-                  width={200}
-                />
-              </div>
-              <div className='animal-info'>
-                <h2>{animal.name}</h2>
-                <p className='short-desc'>{animal.shortDescription}</p>
-                <p className="status">
-                  Status: {getAnimalStatus(animal, { overview: true })}
-                </p>
-              </div>
-            </Link>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <h2 className='animal-header'>Välkommen till djursidan!</h2>
+      <p className='animal-p'>Vänligen, respektera djuren.</p>
+      <div className="animals">
+        <ul>
+          {animals.map((animal) => (
+            <li key={animal.id}>
+              <Link to={`/animals/${animal.id}`}>
+                <div className="image-container">
+                  <img
+                    src={animal.imageUrl}
+                    alt={animal.name}
+                    onError={(e) => (e.currentTarget.src = '')}
+                    width={200}
+                  />
+                </div>
+                <div className="animal-info">
+                  <h2>{animal.name}</h2>
+                  <p className="short-desc">{animal.shortDescription}</p>
+                  <p className="status">
+                    Status: {getAnimalStatus(animal, { overview: true })}
+                  </p>
+                </div>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 };
